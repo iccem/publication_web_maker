55cb0abb52c469fb0fc8ab2b634073a0339c6c16
Lilia Filippova <delo5070@gmail.com>
2024-02-28 11:33:56 +0400

    copier

---
+++ src/Copier.py
@@ -0,0 +1,9 @@
+class Copier:
+    def __int__(self):
+        pass
+
+    def get_copy_file(self):
+        pass
+
+    def get_file_folder(self):
+        pass
--- src/Path_class.py
+++ src/Path_class.py
@@ -9,18 +9,33 @@ class Path:
         папка с цветными фото - путь к папке с файлами (jpg, png, etc.)
 
     Получить
-        preview-публикация (indd-публикация Чб + "легкая" цветная обложка)
+        1) preview-публикация (indd-публикация Чб + "легкая" цветная обложка)
+        требуется: чб-публикация, обложка
     """
+
+    #  Source.
+    bw_indd_file_path = ''
+    cover_hq_files_directory_path = ''
+    photos_directory_path = ''
+
+    def _get_paths(self):
+        """Get paths sources and targets."""
+        dir_source = doc._dir_source + self._current_year + '\\' + self._current_number
+        dir_cover_source = doc._dir_cover_source + '\\' + self._current_number
+
+        bw_indd_file_path = self._current_number + doc._tail_of_filename_publication
+
+        dir_target = doc._dir_target + '\\' + self._current_number
+        dir_target_cover = doc._dir_target + '\\' + self._current_number + '\\' + 'cover'
+
+        path_cover_1_good_txt = doc._path_cover_1_good_txt
 
     def __int__(self, current_year: str, current_number: str):
         self._current_year = current_year
         self._current_number = current_number
+        self._get_paths()
 
-    def get_paths(self):
-        """Get paths sources and targets."""
-        dir_source = doc._dir_source + self._current_year + '\\' + self._current_number
-        dir_cover_source = doc._dir_cover_source + '\\' + self._current_number
-        current_publication = self._current_number + doc._current_publication
-        dir_target = doc._dir_target + '\\' + self._current_number
-        dir_target_cover = doc._dir_target + '\\' + self._current_number + '\\' + 'cover'
-        path_cover_1_good_txt = doc._path_cover_1_good_txt
+
+path = Path('2024', '08')
+
+
