82db86ea284d0738f31439271fa76d8a03a7d15b
Lilia Filippova <delo5070@gmail.com>
2024-03-03 13:44:07 +0400

    copier and current_publication_info_maker

--- src/Copier.py
+++ src/Copier.py
@@ -1,9 +1,42 @@
+import os
+import shutil
+from doc import doc
+
+
+class Current_publication_info_maker:
+    def __int__(self):
+        pass
+
+    def get_artefacts(self, current_number: str):
+        current_publication = current_number + doc._current_publication
+        file_source = doc.dir_source + '\\' + current_publication
+        file_target = doc.dir_target + '\\' + current_publication
+
+
 class Copier:
     def __int__(self):
         pass
 
-    def get_copy_file(self):
-        pass
+    def get_copy_file(self, filefrom: str, fileto: str):
+        """Copy the file."""
+        try:
+            shutil.copy(filefrom, fileto)  # копируем публикацию
+            print('Indd has copied!')
+        except:
+            print('err')
 
-    def get_file_folder(self):
-        pass
+    def get_file_folder(self, dirsource: str, dirto: str):
+        """Copy the cover files folder."""
+        if not os.path.exists(dirto):
+            os.makedirs(dirto)
+        with os.scandir(dirsource) as files_list:
+            for file in files_list:
+                if file.is_file():
+                    print(file.name)
+                    try:
+                        shutil.copy(dirsource + '\\' + file.name, dirto + '\\' + file.name)
+                        # копируем обложку
+                        #  обложка большая! копируется медленно!
+                        print('Cover has copied!')
+                    except Exception as e:
+                        print('Export to PDF failed: ' + str(e))
