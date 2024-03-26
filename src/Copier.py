8371ca413c77fe16fa29f04447e763a82487a7e9
Lilia Filippova <delo5070@gmail.com>
2024-03-26 10:08:38 +0400

    refactor copy_cover_files function for better clarity, efficiency, and error handlin

--- src/Copier.py
+++ src/Copier.py
@@ -69,18 +69,30 @@ class Copier:
                     except Exception as e:
                         print('Export to PDF failed: ' + str(e))
 
-    def get_title_cover_file(self, cover_dir_source: str, cover_dir_target: str) -> None:
-        """Copy the file folder."""
-        if not os.path.exists(cover_dir_target):
-            os.makedirs(cover_dir_target)
-        with os.scandir(cover_dir_source) as files_list:
-            for file in files_list:
-                if file.is_file():
-                    print(file.name)
-                    try:
-                        shutil.copy(cover_dir_source + '\\' + file.name, cover_dir_target + '\\' + file.name)
-                        # копируем обложку
-                        #  обложка большая! копируется медленно!
-                        print('Cover has copied!')
-                    except Exception as e:
-                        print('Export to PDF failed: ' + str(e))
+    def copy_cover_files(self, source_dir: str, target_dir: str) -> None:
+        """
+        Copy cover files from the source directory to the target directory.
+
+        Args:
+            source_dir (str): The path to the source directory containing cover files.
+            target_dir (str): The path to the target directory where cover files will be copied.
+
+        Returns:
+            None
+        """
+        # Create the target directory if it doesn't exist
+        os.makedirs(target_dir, exist_ok=True)
+
+        # Iterate over files in the source directory
+        for file_name in os.listdir(source_dir):
+            source_file_path = os.path.join(source_dir, file_name)
+            target_file_path = os.path.join(target_dir, file_name)
+
+            # Check if the current item is a file
+            if os.path.isfile(source_file_path):
+                try:
+                    # Copy the file to the target directory
+                    shutil.copy(source_file_path, target_file_path)
+                    print(f"Cover file '{file_name}' has been copied successfully.")
+                except Exception as e:
+                    print(f"Error copying cover file '{file_name}': {str(e)}")
