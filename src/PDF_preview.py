98f696db113791ced7784f0f717f6b76c9f6cecb
Lilia Filippova <delo5070@gmail.com>
2024-06-28 09:34:48 +0400

    build pdf-review creater

---
+++ src/PDF_preview.py
@@ -0,0 +1,14 @@
+class PDF_preview:
+    """
+    Creates a preview version of the publication
+    and sends it to subscribers.
+    """
+
+    def __int__(self, number):
+        pass
+
+    def create(self):
+        self
+
+    def send(self):
+        pass
